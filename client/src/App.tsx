9a6d3536326357c7da4ec9cfda7c049ab5d53736
Austin Abhari <aabmxer@gmail.com>
2025-01-17 12:45:57 -0700

    People block

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { PeopleProvider } from "./providers/PeopleProvider";
+import PeopleBlock from "./components/PeopleBlock";
 
 function App() {
 	return (
 		<div className="App">
 			<PeopleProvider>
-				<div />
+				<PeopleBlock />
 			</PeopleProvider>
 		</div>
 	);
---
+++ client/src/components/PeopleBlock.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { usePeople } from "../providers/PeopleProvider";
+
+interface Person {
+	name: string;
+	age: number;
+}
+
+const PeopleBlock: React.FC = () => {
+	const { people, isLoading, error } = usePeople();
+
+	if (isLoading) {
+		return <div>HOLD YOUR HORSES PARTNER</div>;
+	}
+
+	if (error) {
+		return <div> OH NO PARTNER: {error}</div>;
+	}
+
+	return (
+		<>
+			{people.map((person: Person) => (
+				<>
+					<div> {person.name}</div>
+					<div> {person.age} </div>
+				</>
+			))}
+		</>
+	);
+};
+
+export default PeopleBlock;
